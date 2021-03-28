fca00424b4372a79624a3d043eb5643aacb186a9
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-03-27 19:21:47 -0600

    working on graph class

---
+++ graph.py
@@ -0,0 +1,13 @@
+class Node:
+	def __init__(self, point, neighbors = None):
+		self.point = point
+		self.neighbors = neighbors
+
+	def get_neighbors(self):
+		return self.neighbors
+
+
+if __name__ == '__main__':
+
+	graph = Node((1,0)) 
+	print(graph.get_neighbors())
